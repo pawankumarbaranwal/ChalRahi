baef69474e982be1377a001d98b9e1b2fb11b9e4
pawankumarbaranwal <pawankumarbaranwal@gmail.com>
2015-08-30 02:50:27 +0530

    Initial Commit

--- app/src/main/java/example/android/com/chalrahi/InviteAndEarnFragment.java
+++ app/src/main/java/example/android/com/chalrahi/InviteAndEarnFragment.java
@@ -1,18 +1,62 @@
 package example.android.com.chalrahi;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class InviteAndEarnFragment extends Fragment{
+public class InviteAndEarnFragment extends Fragment implements View.OnClickListener
+{
 
     private View view;
+    private Button inviteAndEarn;
+    private TextView inviteMessage;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
         view = inflater.inflate(R.layout.fragment_invite_and_earn, container, false);
+        initializeData();
+        setData();
         return view;
+    }
+
+    private void initializeData()
+    {
+            inviteMessage=(TextView)view.findViewById(R.id.tvInviteMessage);
+            inviteAndEarn=(Button)view.findViewById(R.id.btnInvite);
+            inviteAndEarn.setOnClickListener(this);
+    }
+
+    private void setData() {
+        StringBuilder message = new StringBuilder("Help your Friends \n");
+        message.append("Invie your friend to download the app \n");
+        message.append("You and your friend will get Rs.100 each\n");
+        message.append("once your friend completes first auto ride\n");
+        message.append("Your unique code is  \n");
+        inviteMessage.setText(message.toString());
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        if (v==inviteAndEarn){
+            try
+            { Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("text/plain");
+                i.putExtra(Intent.EXTRA_SUBJECT, "My application name");
+                String messageToBeSent = "\nLet me recommend you this application \n";
+                messageToBeSent = messageToBeSent+ "https://play.google.com/store/apps/details?id=Orion.Soft \n";
+                i.putExtra(Intent.EXTRA_TEXT, messageToBeSent);
+                startActivity(Intent.createChooser(i, "Choose One"));
+            }
+            catch(Exception e)
+            { //e.toString();
+            }
+        }
     }
 }
--- app/src/main/java/example/android/com/chalrahi/MainActivity.java
+++ app/src/main/java/example/android/com/chalrahi/MainActivity.java
@@ -2,25 +2,35 @@ package example.android.com.chalrahi;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Context;
 import android.content.res.Configuration;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends ActionBarActivity implements AdapterView.OnItemClickListener {
 
     private DrawerLayout drawerlayout;
     private ListView listview;
-    private String[] ListItem;
+    private List<String> drawerItemList;
     private ActionBarDrawerToggle drawerListener;
     private FragmentTransaction fragmentTransaction;
     private FragmentManager fragmentManager;
+    private Context context=this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +38,11 @@ public class MainActivity extends ActionBarActivity implements AdapterView.OnIte
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         drawerlayout = (DrawerLayout)findViewById(R.id.drawerLayout);
-        ListItem = getResources().getStringArray(R.array.fragment_drawer_items);
+        drawerItemList = Arrays.asList(getResources().getStringArray(R.array.fragment_drawer_items));
         listview = (ListView)findViewById(R.id.drawerList);
         listview.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-
-        listview.setAdapter(new ArrayAdapter<>(this,android.R.layout.simple_list_item_activated_1,ListItem));
+        AdapterClass adapter=new AdapterClass();
+        listview.setAdapter(adapter);
 
         listview.setOnItemClickListener(this);
 
@@ -142,12 +152,67 @@ public class MainActivity extends ActionBarActivity implements AdapterView.OnIte
 
     public void selectItem(int position)
     {
-        listview.setItemChecked(position,true);
-        setTitle(ListItem[position]);
+        listview.setItemChecked(position, true);
+        setTitle(drawerItemList.get(position));
     }
 
     public void setTitle(String title)
     {
         getSupportActionBar().setTitle(title);
     }
+
+
+    private class AdapterClass extends BaseAdapter{
+
+
+        @Override
+        public int getCount() {
+            Log.i("error","1");
+            return drawerItemList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            Log.i("error","2");
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            Log.i("error","3");
+            return 0;
+        }
+        class ViewHolder {
+            TextView drawerItem;
+            ViewHolder(View view) {
+                Log.i("error","4");
+                drawerItem = (TextView) view.findViewById(R.id.tvDrawerItem);
+                Log.i("error","5");
+            }
+        }
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder viewHolder;
+            View row = convertView;
+            if (row == null) {
+                Log.i("error","6");
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                Log.i("error","7");
+                row=inflater.inflate(R.layout.row_for_fragment_item, parent, false);
+                Log.i("error","8");
+                viewHolder=new ViewHolder(row);
+                Log.i("error","9");
+                row.setTag(viewHolder);
+            }else{
+                Log.i("error","10");
+                viewHolder=(ViewHolder)row.getTag();
+            }
+            //Customer c=customerList.get(position);
+            String itemName = drawerItemList.get(position);
+            viewHolder.drawerItem.setText(itemName);
+            //viewHolder.nameTextView.setTag(c);
+            return row;
+        }
+    }
+
 }
