ad9261af98ada96b5f57d3fb189718c0acccb3ab
pawankumarbaranwal <pawankumarbaranwal@gmail.com>
2015-09-01 02:50:36 +0530

    Okkhttp changes

--- app/src/main/java/example/android/com/chalrahi/LoginActivity.java
+++ app/src/main/java/example/android/com/chalrahi/LoginActivity.java
@@ -3,10 +3,16 @@ package example.android.com.chalrahi;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.Map;
 
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
@@ -15,13 +21,13 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private Button register;
     private EditText mobileNumber;
     private EditText password;
+    String response;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         initializeView();
-
     }
 
     private void initializeView()
@@ -37,14 +43,28 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     public void onClick(View v)
     {
+        OkHttpHandler handler = new OkHttpHandler();
         Intent intent;
         Validator validator =new Validator();
         if (v==login){
             try
             {
                 validator.validateLoginDetails(mobileNumber.getText().toString(),password.getText().toString());
+                    response = handler.execute().get();
+                    if (response != null && response != "")
+                    {
+                        Log.i("reponse", response);
+                        Gson gson = new GsonBuilder().create();
+                        Map<String, Object> mapObject = new Gson().fromJson(response, new TypeToken<Map<String, Object>>() {}.getType());
+                        Log.i("ResponseType",mapObject.get("error")+"");
+                        if (mapObject.get("error").toString().equals("true"))
+                        {
+                            throw new Exception(mapObject.get("error_msg")+"");
+                        }
+                    }
                 intent=new Intent(this,UserHomeActivity.class);
                 startActivity(intent);
+                //return pnr;
             } catch (Exception e) {
                 Toast.makeText(this,e.getMessage(),Toast.LENGTH_SHORT).show();
             }
---
+++ app/src/main/java/example/android/com/chalrahi/OkHttpHandler.java
@@ -0,0 +1,41 @@
+package example.android.com.chalrahi;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+
+public class OkHttpHandler extends AsyncTask<Void, Void, String> {
+
+    // TODO: replace with your own image url
+    private final String REQUEST_URL = "http://app-login.herokuapp.com/index.php";
+
+    OkHttpClient httpClient = new OkHttpClient();
+
+    public static final MediaType JSON
+            = MediaType.parse("application/x-www-form-urlencoded; charset=utf-8");
+
+    @Override
+    protected String doInBackground(Void... params) {
+
+        String parameters = "email=pawan@gmail.com&password=qwert1y&tag=login";
+        RequestBody reqBody = RequestBody.create(JSON,parameters);
+        Request.Builder builder = new Request.Builder();
+        builder.url(REQUEST_URL).post(reqBody);
+        Request request = builder.build();
+
+        try
+        {
+            Response response = httpClient.newCall(request).execute();
+            return response.body().string();
+        } catch (Exception e)
+        {
+
+        }
+        return null;
+    }
+}
